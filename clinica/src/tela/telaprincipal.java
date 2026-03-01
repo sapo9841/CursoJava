1c54721b09a7b353033c09eb666574a622d656ed
Marcus Brazão <marcusbrzao@gmail.com>
2026-03-01 06:19:23 -0300

    comentando codigo

--- clinica/src/tela/telaprincipal.java
+++ clinica/src/tela/telaprincipal.java
@@ -1,5 +1,8 @@
 package tela;
 
 public class telaprincipal {
-
+	//O main é o chefe
+public static void main(String[] args) {
+	
+}
 }
--- exercicios/src/fundamentos/PrimeiroProjeto.java
+++ exercicios/src/fundamentos/PrimeiroProjeto.java
@@ -1,8 +1,20 @@
 package fundamentos;
 
 public class PrimeiroProjeto {
+ /* Com 2 barras comentamos
+	*MAIN é o chefe
+    *control + barra = comenta
+ */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		//uma centença de codigo termina com ;
+		System.out.println("Hello World!"); //classico hello world ;)
+/**
+ * essa classe representa.... JAVADOC
+ * @author Marcus <@gmail.com>
+ * @since JDK 1.0
+ * @see
+ */
 	}
 
 }
